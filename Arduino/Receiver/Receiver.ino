705b8ffaaf864ed94a582dd2e99e23d363e8570f
paul <s1126779@sms.ed.ac.uk>
2015-02-10 18:34:17 +0000

    added negative signs for backwards speeds in RUN_ENGINE to fix negative speed bug

--- Arduino/Receiver/Receiver.ino
+++ Arduino/Receiver/Receiver.ino
@@ -1,36 +1,29 @@
-#include <SDPArduino.h>
-
-
+#include <SerialCommand.h>
 #include <SDPArduino.h>
 #include <Wire.h>
 #include <SerialCommand.h>
-#include <SoftwareSerial.h>   // We need this even if we're not using a SoftwareSerial object
-                              // Due to the way the Arduino IDE compiles
+#include <SoftwareSerial.h>   // We need this even if we're not using a SoftwareSerial object due to the way the Arduino IDE compiles
 
-#define LED_PIN 13   // Arduino LED on board
+
+#define LED_PIN 13  // Arduino LED on board
 #define RADIO_PIN 8
 #define LEFT_WHEEL_MOTOR 3
 #define RIGHT_WHEEL_MOTOR 5
 #define KICKER_MOTOR 4
 #define CATCHER_MOTOR 2
 
-// int left_speed;
-// int right_speed;
-// int left_dir;
-// int right_dir;
-
 SerialCommand SCmd;   // The demo SerialCommand object
 
 void setup() {
 
   SDPsetup();
-  //helloWorld();
   pinMode(LED_PIN, OUTPUT);   // initialize pin 13 as digital output (LED)
   pinMode(8, OUTPUT);    // initialize pin 8 to control the radio
   digitalWrite(8, HIGH); // select the radio
   Serial.begin(115200);    // start the serial port at 115200 baud (correct for XinoRF and RFu, if using XRF + Arduino you might need 9600)
   
   Serial.print("STARTED");
+  
   // !!!
   // Make sure there's no more than ten of these!
   // Any more and the bottom ones don't work
@@ -39,7 +32,6 @@ void setup() {
   SCmd.addCommand("RUN_KICK",kick);            
   SCmd.addCommand("RUN_CATCH", pick_up);         
   SCmd.addCommand("DROP", drop);
-  /*SCmd.addCommand("SET_ENG", SET_ENGINE);*/
   SCmd.addCommand("RUN_ENG", RUN_ENGINE);
   SCmd.addDefaultHandler(unrecognized);
 }
@@ -48,23 +40,6 @@ void loop() {
   SCmd.readSerial();
 }
 
-/*void SET_ENGINE() {
-
-  char *lftspd = SCmd.next();
-  char *rgtspd = SCmd.next();
-  
-  
-  if (lftspd != NULL && rgtspd != NULL) {
-    left_speed = atoi(lftspd);
-    right_speed = atoi(rgtspd);
-    motorForward(5, left_speed);
-    motorForward(3, right_speed);
-    delay(1000);
-    motorStop(3);
-    motorStop(5);
-  }
- 
-}*/
 
 void RUN_ENGINE() {
 
@@ -134,22 +109,26 @@ void pick_up() {
   motorStop(CATCHER_MOTOR);
 }
 
+
 void drop() {
   motorForward(CATCHER_MOTOR,100);
   delay(300);
   motorStop(CATCHER_MOTOR);
 }
 
+
 void unrecognized() {
   
   Serial.println("What?"); 
 }
 
-// LED functions for testing
+
+/*-----------------------LED functions for testing-------------------*/
 void LED_on() {
   Serial.println("LED on"); 
   digitalWrite(LED_PIN,HIGH);  
 }
+
 
 void LED_off() {
   
@@ -157,6 +136,7 @@ void LED_off() {
   digitalWrite(LED_PIN,LOW);
   
 }
+
 
 void flash(int interval) {
   
